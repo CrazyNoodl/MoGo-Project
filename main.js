48cac47de950fa5dcd0e694f90005b9010ffab59
Rudenko Artem <artem.rud@bk.ru>
2019-06-05 19:23:06 +0300

    js

--- main.js
+++ main.js
@@ -29,4 +29,33 @@ $(function () {
         }
     }
 
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav, a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
+
+    /*Nav*/
+    $("#burger").on("click", function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+
+
+    })
+
+
+
 });
